feb1c1e768b226ec0fb40ffc19be02600298373f
Mauricio Vasquez Villanueva <mauriciovasquezvillanueva@gmail.com>
2023-05-19 16:35:12 -0500

    controlador api

--- routes/api.php
+++ routes/api.php
@@ -19,5 +19,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
+Route::middleware('auth')->group(function () {
 Route::get('/estudiantes',[EstudianteController::class, 'index'])->name('estudiantes');
+});
