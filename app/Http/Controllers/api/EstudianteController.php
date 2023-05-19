948160d530d5c23e14e032264ba8305ce84bb347
Mauricio Vasquez Villanueva <mauriciovasquezvillanueva@gmail.com>
2023-05-19 16:10:13 -0500

    Controlador api estudiante

---
+++ app/Http/Controllers/api/EstudianteController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Estudiante;
+use Illuminate\Support\Facades\DB;
+
+class EstudianteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+    $estudiante =DB::table('estudiantes')
+    ->join('carrera', 'estudiantes.id_Carrera','=','carrera.id_Carrera')
+    ->select('estudiantes.*',"carrera.Carrera")
+    ->get();
+    return json_encode(['estudiante'=>$estudiante]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
