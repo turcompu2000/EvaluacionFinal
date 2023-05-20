5eb7dc3ed21d5f6d5c9baaa129195bab37b095f5
Mauricio Vasquez Villanueva <mauriciovasquezvillanueva@gmail.com>
2023-05-20 01:39:37 -0500

    Controlador api Carrera

---
+++ app/Http/Controllers/api/CarreraController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Carrera;
+use Illuminate\Support\Facades\DB;
+
+class CarreraController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $carrera=DB::table('carrera')
+        ->orderBy('Carrera')
+        ->get();
+        return json_encode(['carrera'=>$carrera]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
